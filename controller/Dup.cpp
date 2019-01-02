f4a7e50f30ff51f018923296e6907eed5066ffd7
Chani <632chani@gmail.com>
2019-01-02 11:24:59 -0800

    fix default file

--- controller/Dup.cpp
+++ controller/Dup.cpp
@@ -4,24 +4,48 @@
 
 #include "../controller/Dup.h"
 
+std::string getName(int number, DnaData & data)
+{
+    static int id = 1;
+    std::string name = data.getNameById(number);
+    std::stringstream ss;
+    ss << name << "_" << id++;
+    name = ss.str();
+    return name;
+}
+
 void Dup::action(std::list<std::string> args, DnaData & data)
 {
-    std::string s = args.front();
+    std::string s = args.front(); //dup from
     std::string del = s.substr(0, 1);
-    std::string name = args.back().substr(1,args.back().length());
 
+    std::string name;
     if (del == "#")
     {
         int number = fromString(s.substr(1, s.length()));
+        if (args.size() > 1)
+            name = args.back().substr(1,args.back().length()); // name copy
+        else
+            name = getName(number, data);
         data.newDnaByDna(name, data.getByNumber(number));
     }
     else
     {
-        std::string name_of_dna = s.substr(1, s.length());
-        data.newDnaByDna(name, data.getByName(name_of_dna));
+        if (del != "@")
+        {
+            throw "Invalid Name Of Sequence\n";
+        }
+        std::string seqName = s.substr(1, s.length());
+        if (args.size() > 1)
+            name = args.back().substr(1,args.back().length());
+        else
+            name = getName(data.getIdByName(seqName), data);
+        data.newDnaByDna(name, data.getByName(name));
     }
 
     std::stringstream ss;
     ss << "[" << data.getIdByName(name) << "] " << name <<": " << data.getIdByName(name) << "\n";
     m_message = ss.str();
 }
+
+
--- controller/Save.cpp
+++ controller/Save.cpp
@@ -5,21 +5,46 @@
 #include "../controller/Save.h"
 #include "../model/DNAWriter.h"
 
+std::string getFileName(std::string name)
+{
+    std::stringstream ss;
+    ss << name << ".rawdna";
+    name = ss.str();
+    return name;
+}
 void Save::action(std::list<std::string> args, DnaData & data)
 {
-    std::string s = args.front();
+    std::string s = args.front();   // seq name
     std::string del = s.substr(0, 1);
-    std::string name = args.back().substr(1,args.back().length());
-    DNAWriter writer(args.back());
+
+    std::string name = args.back().substr(1,args.back().length()); // file name
+    //.rawdna
+
+
     if (del == "#")
     {
         int number = fromString(s.substr(1, s.length()));
-        writer.Write(data.getByNumber(number).getDna());
+        if (args.size() > 1) {
+            DNAWriter writer(args.back());
+            writer.Write(data.getByNumber(number).getDna());
+        }
+        else {
+            DNAWriter writer(getFileName(data.getNameById(number)));
+            writer.Write(data.getByNumber(number).getDna());
+        }
+
     }
     else
     {
         std::string name_of_dna = s.substr(1, s.length());
-        writer.Write(data.getByName(name_of_dna).getDna());
+        if (args.size() > 1) {
+            DNAWriter writer(args.back());
+            writer.Write(data.getByName(name_of_dna).getDna());
+        }
+        else {
+            DNAWriter writer(getFileName(name_of_dna));
+            writer.Write(data.getByName(name_of_dna).getDna());
+        }
     }
 
 }
