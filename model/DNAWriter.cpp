42abdee48cdcd3d7068cc9ebe04568c4a3eb273a
Chani <632chani@gmail.com>
2019-01-05 14:31:46 -0800

    change in dna writer

--- model/DNAWriter.cpp
+++ model/DNAWriter.cpp
@@ -4,10 +4,13 @@
 
 #include "../model/DNAWriter.h"
 #include "../model/dna_seq.h"
+#include "../controller/ERROR_CODES.h"
 
 DNAWriter::DNAWriter(std::string name)
 {
     myfile.open(name.c_str());
+    if (!myfile.is_open())
+        throw FILE_NOT_FOUND;
 }
 void DNAWriter::Write(const DNA & other)
 {
