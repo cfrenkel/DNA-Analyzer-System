b6e97b94adf2b116a1fca9f20706e339179d5023
Chani <632chani@gmail.com>
2019-01-16 22:34:32 -0800

    findall command

---
+++ controller/commands/anlaysis/FindAll.cpp
@@ -0,0 +1,55 @@
+//
+// Created by cfrenkel on 1/16/19.
+//
+
+#include "FindAll.h"
+
+//
+// Created by cfrenkel on 1/16/19.
+//
+
+#include <algorithm>
+#include "FindAll.h"
+
+
+
+#include "../../admin/Convert.h"
+#include "../../factory/FactoryCommand.h"
+#include "../../errorManagment/ERROR_CODES.h"
+
+bool FindAll::reg = FactoryCommand::registerCommand("findall", SharePointer<Command>(new FindAll));
+
+std::string FindAllSubString(std::string main, std::string sub)
+{
+    int occurrences = 0;
+    std::string::size_type pos = 0;
+    std::stringstream ss;
+
+    while ((pos = main.find(sub, pos )) != std::string::npos) {
+        ss << main.find(sub, pos ) << " ";
+        pos += sub.length();
+//      pos += 1;
+
+    }
+    ss << "\n";
+    return ss.str();
+}
+
+void FindAll::action(std::list<std::string> args, DnaData & data)
+{
+    if (args.size() < 1 || args.size() > 2)
+        throw INVALID_COMMAND;
+
+    // > cmd >>> find <seq> <expressed_sub_seq>
+
+    DnaMetaData & dnaMetaData = data.getDnaByArgs(args.front());
+    std::string main_seq = dnaMetaData.getSeqStringDnaWithPar(dnaMetaData.getSharePointerDna()->getLength());
+    std::string sub_seq = args.back();
+
+    if (sub_seq.substr(0,1) == "#" || sub_seq.substr(0,1) == "@")
+        sub_seq = data.getDnaByArgs(sub_seq).getSeqStringDnaWithPar(data.getDnaByArgs(sub_seq).getSharePointerDna()->getLength());
+
+    std::transform(sub_seq.begin(), sub_seq.end(),sub_seq.begin(), ::toupper);
+
+    m_message = FindAllSubString(main_seq, sub_seq);
+}
---
+++ controller/commands/anlaysis/FindAll.h
@@ -0,0 +1,20 @@
+//
+// Created by cfrenkel on 1/16/19.
+//
+
+#ifndef EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_FINDALL_H
+#define EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_FINDALL_H
+
+
+#include "../Command.h"
+
+class FindAll :public Command
+{
+    static bool reg;
+public:
+    void action(std::list<std::string>, DnaData &);
+};
+
+
+
+#endif //EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_FINDALL_H
