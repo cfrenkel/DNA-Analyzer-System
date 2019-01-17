247dd8a2e59a5c8530953921ee2f0dc6e5a7c8a8
Chani <632chani@gmail.com>
2019-01-16 21:43:57 -0800

    len command

---
+++ controller/commands/anlaysis/Len.cpp
@@ -0,0 +1,23 @@
+//
+// Created by cfrenkel on 1/3/19.
+//
+
+#include "Len.h"
+#include "../../admin/Convert.h"
+#include "../../factory/FactoryCommand.h"
+#include "../../errorManagment/ERROR_CODES.h"
+
+bool Len::reg = FactoryCommand::registerCommand("len", SharePointer<Command>(new Len));
+
+void Len::action(std::list<std::string> args, DnaData & data)
+{
+    if (args.size() > 1)
+        throw INVALID_COMMAND;
+
+
+    int len = data.getDnaByArgs("#" + args.front()).getSharePointerDna()->getLength();
+
+    std::stringstream ss;
+    ss << len << "\n";
+    m_message = ss.str();
+}
---
+++ controller/commands/anlaysis/Len.h
@@ -0,0 +1,18 @@
+//
+// Created by cfrenkel on 1/16/19.
+//
+
+#ifndef EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_LEN_H
+#define EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_LEN_H
+
+
+#include "../Command.h"
+
+class Len :public Command
+{
+    static bool reg;
+public:
+    void action(std::list<std::string>, DnaData &);
+};
+
+#endif //EXCELLENTEAM_ELLA_C_DNA_CFRENKEL_LEN_H
